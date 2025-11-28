edaf726fb2f0686b4165167a1153ef5e91d10a42
Juan Ortega <jmo.narvaez@gmail.com>
2025-11-27 22:52:45 -0500

    🔐 chore: ajusto configuración de seguridad del gateway

--- backend/gateway/src/main/java/co/edu/unicauca/gateway/config/SecurityConfig.java
+++ backend/gateway/src/main/java/co/edu/unicauca/gateway/config/SecurityConfig.java
@@ -2,6 +2,36 @@ package co.edu.unicauca.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .cors(ServerHttpSecurity.CorsSpec::disable)
+
+
+                .authorizeExchange(ex -> ex
+                        .anyExchange().permitAll()
+                )
+
+
+                .build();
+    }
+}
+
+
+/*
+package co.edu.unicauca.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -91,3 +121,4 @@ public class SecurityConfig {
         };
     }
 }
+*/

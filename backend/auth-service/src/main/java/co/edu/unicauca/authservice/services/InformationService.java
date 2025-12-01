ce637406910af1e6125b4350bdf6bc06e13a65ba
LauraMolano <lauraisamolano@unicauca.edu.co>
2025-12-01 12:32:04 -0500

    feat: ✨ actualizar lógica de información y repositorio de jefe en auth-service

--- backend/auth-service/src/main/java/co/edu/unicauca/authservice/services/InformationService.java
+++ backend/auth-service/src/main/java/co/edu/unicauca/authservice/services/InformationService.java
@@ -1,8 +1,11 @@
 package co.edu.unicauca.authservice.services;
 
 import co.edu.unicauca.authservice.access.CoorRepository;
+import co.edu.unicauca.authservice.access.JefeRepository;
 import co.edu.unicauca.authservice.access.PersonaRepository;
 import co.edu.unicauca.authservice.domain.entities.Coordinador;
+import co.edu.unicauca.authservice.domain.entities.JefeDeDepartamento;
+import co.edu.unicauca.shared.contracts.model.Departamento;
 import co.edu.unicauca.shared.contracts.model.Programa;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,9 +20,17 @@ public class InformationService {
     @Autowired
     private PersonaRepository personaRepository;
 
+    @Autowired
+    private JefeRepository jefeRepository;
+
     public String obtenerEmailCoordinadorPorPrograma (String programa){
         Coordinador coor = coorRepository.findByPrograma(Programa.valueOf(programa)).get();
         return coor.getUsuario().getEmail();
+    }
+
+    public String obtenerEmailJefePorDepartamento (String departamento){
+        JefeDeDepartamento jefe = jefeRepository.findByDepartamento(Departamento.valueOf(departamento)).get();
+        return jefe.getUsuario().getEmail();
     }
 
     public String obtenerTelefonoPorCorreo(String correo) {
